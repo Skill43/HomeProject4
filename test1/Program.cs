eb50ad185ee558455eb44bf725ffca3d107186bb
Илья <ilacikoridze357@gmail.com>
2022-12-15 11:06:37 +0300

    Количество четных чисел в массиве

---
+++ test1/Program.cs
@@ -0,0 +1,12 @@
+﻿const int Size = 5;
+const int LeftRange = 100;
+const int RightRange = 999;
+int[] array = new int[Size];
+Random rand = new Random();
+int res =0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(LeftRange, RightRange + 1);
+    if (array[i] % 2 == 0) res++;
+}
+System.Console.WriteLine($"Количество четных чисел в массиве: \n| {(string.Join(" | ", array))} | = {res}");
