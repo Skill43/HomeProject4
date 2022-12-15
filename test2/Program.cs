09843130d4d942f967a178bba0b1f0552625652f
Илья <ilacikoridze357@gmail.com>
2022-12-15 11:54:15 +0300

    Сумма элементов массива, стоящих на нечётных позициях

---
+++ test2/Program.cs
@@ -0,0 +1,13 @@
+﻿const int Size = 9;
+const int LeftRange = -9;
+const int RightRange = 9;
+
+int[] array = new int[Size];
+Random rand = new Random();
+int res =0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(LeftRange, RightRange + 1);
+    if (i % 2 != 0) res += array[i];
+}
+System.Console.WriteLine($"Сумма элементов массива, стоящих на нечётных позициях: \n| {(string.Join(" | ", array))} | = {res}");
